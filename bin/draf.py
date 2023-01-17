e8864143db7b8079fd12d9c0baf1477fc1ca305a
Rodo <rodonguyendd@gmail.com>
2023-01-18 01:41:00 +1000

    Major refactor

--- bin/draf.py
+++ bin/draf.py
@@ -1,21 +1,24 @@
 import heapq
 
-lll = [2,5,2,8,3,7,9,3,2,99]
-new = []
+# lll = [2,5,2,8,3,7,9,3,2,99]
+# new = []
 
-for i in lll:
-  heapq.heappush(new, i)
-  heapq.heapify(new)
-  print(new)
+# for i in lll:
+#   heapq.heappush(new, i)
+#   heapq.heapify(new)
+#   print(new)
 
-heapq.heapreplace(new, 5)
-print(new)
+# heapq.heapreplace(new, 5)
+# print(new)
 
 
-print(new)
+# print(new)
 
-heapq.heapify(lll)
-m = heapq.nsmallest(7, lll)
+# heapq.heapify(lll)
+# m = heapq.nsmallest(7, lll)
 
-print(m)
-print(sorted(lll))
+# print(m)
+# print(sorted(lll))
+
+
+print()
